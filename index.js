020a6eccd8cb4217c9bbcd2dff291bcf24cbe295
Subrat Mallick <m.subrat198470@gmail.com>
2022-05-23 15:03:19 +0530

    doneh

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
     function updateVisitCount() {
         fetch('https://api.countapi.xyz/update/subbrat/escapegame/?amount=+1')
+            // fetch('https://api.countapi.xyz/get/subbrat/escapegame/')
             .then(res => res.json())
             .then(res => {
                 countE0.innerHTML = res.value;
